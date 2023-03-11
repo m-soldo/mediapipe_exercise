ba778ae51b1e4bd6c2f4ddd25016b47f294fc485
m-soldo <100207531+m-soldo@users.noreply.github.com>
2023-03-11 11:04:22 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import cv2 as cv
 import mp_exercise_modul as mem
 
-
-cap = cv.VideoCapture('1.mp4')
-# width, height = cap.get(3), cap.get(4)
+# give wanted video path as an argument or 0 for webcam
+cap = cv.VideoCapture('some_folder/some_video.mp4')
+# name of the exercise, must be pullup, pushup or squat
 exercise = 'pullup'
 
-if exercise == 'pullup':
+if exercise.lower() == 'pullup':
     counter = 0
     phase = None
-elif exercise == 'pushup':
+elif exercise.lower() == 'pushup':
     counter = -1
     phase = "UP"
-elif exercise == 'squat':
+elif exercise.lower() == 'squat':
     counter = 0
     phase = "DOWN"
 else:
